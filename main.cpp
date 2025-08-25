3a01c61cf12c12ddcd4b65692177b6d4d653a2c3
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-08-25 20:22:29 +0300

    spelling fixed

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ using namespace std;
 
 #define NC 47
 
-ifstream fin("scor.txt");
+ifstream fin("score.txt");
 
-vector<int> scors[NC];
+vector<int> scores[NC];
 void load(){
     string line="";
     while(getline(fin,line)){
@@ -21,50 +21,50 @@ void load(){
         sin>>lineNR>>c;
         --lineNR;
         int nr;
-        while(sin>>nr)scors[lineNR].push_back(nr);
+        while(sin>>nr)scores[lineNR].push_back(nr);
     }
 }
 int next_test(){
     struct element{
-        int index, last_scor;
-        size_t scor_size;
-        element(int _index,int _last_scor,size_t _scor_size){
+        int index, last_score;
+        size_t score_size;
+        element(int _index,int _last_score,size_t _score_size){
             index=_index;
-            last_scor=_last_scor;
-            scor_size=_scor_size;
+            last_score=_last_score;
+            score_size=_score_size;
         }
     };
-    vector<element>last_scors;
-    for(int i=0;i<NC;++i)last_scors.push_back(element(i+1,scors[i].back(),scors[i].size()));
-    sort(last_scors.begin(),last_scors.end(),[](element p1,element p2){
-        if(p1.last_scor!=p2.last_scor)return p1.last_scor<p2.last_scor;
-        if(p1.scor_size!=p2.scor_size)return p1.scor_size<p2.scor_size;
+    vector<element>last_scores;
+    for(int i=0;i<NC;++i)last_scores.push_back(element(i+1,scores[i].back(),scores[i].size()));
+    sort(last_scores.begin(),last_scores.end(),[](element p1,element p2){
+        if(p1.last_score!=p2.last_score)return p1.last_score<p2.last_score;
+        if(p1.score_size!=p2.score_size)return p1.score_size<p2.score_size;
         return rand()%14%2==0;
     });
-    //cout<<last_scors[0].last_scor<<" - ";
-    return last_scors[0].index;
+    //cout<<last_scores[0].last_score<<" - ";
+    return last_scores[0].index;
 }
-void update(int testNR,int newScore){
+void update(int testNR,int newscore){
     --testNR;
-    scors[testNR].push_back(newScore);
+    scores[testNR].push_back(newscore);
     fin.close();
-    ofstream fout("scor.txt");
-    for(int i=0;i<NC;++i){fout<<i+1<<": ";for(auto it:scors[i])fout<<it<<" ";fout<<"\n";}
+    ofstream fout("score.txt");
+    for(int i=0;i<NC;++i){fout<<i+1<<": ";for(auto it:scores[i])fout<<it<<" ";fout<<"\n";}
 }
 int main(){
     srand(time(0));
     load();
-    //for(int i=0;i<NC;++i){for(auto it:scors[i])cout<<it<<" ";cout<<"\n";}
-    cout<<"1 - get next test to redo\n2 - add new scor\n3 - kill program\n";
+    //for(int i=0;i<NC;++i){for(auto it:scores[i])cout<<it<<" ";cout<<"\n";}
+    cout<<"1 - get next test to redo\n2 - add new score\n3 - kill program\n";
     int select;
     cin>>select;
     if(select==1){
         cout<<"next test: "<<next_test();
     }else if(select==2){
-        cout<<"enter test number and scor\n";
-        int testNR,newScore;
-        cin>>testNR>>newScore;
-        update(testNR,newScore);
+        cout<<"enter test number and score\n";
+        int testNR,newscore;
+        cin>>testNR>>newscore;
+        update(testNR,newscore);
     }
     return 0;
 }
